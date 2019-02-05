cc40f74fd3fd8536a0d111d38dfe0515b49155d5
syagawa <syagawapp@gmail.com>
2019-02-05 12:38:44 +0900

    Modify numToArray

--- index.js
+++ index.js
@@ -490,16 +490,16 @@
 
   // 配列での計算
   K.numToArray = function(n){
-    if(!S.isNumber(n)){
-      return "This function has been called with incorrect parameters";
-    }
 
     var arr = [];
     var str = String(n);
     var len = str.length;
     for(var i = 0; i < len; i++){
-      arr.push(str.slice(i, i + 1));
-      arr[i] = Number(arr[i]);
+      var elm = Number(str.slice(i, i + 1));
+      if(!S.isNumber(elm)){
+        return "This function has been called with incorrect parameters";
+      }
+      arr.push(elm);
     }
 
     return arr;
