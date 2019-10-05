4f6a8f0f6b0178c485769d1de2eefcc99c109431
syagawa <syagawapp@gmail.com>
2019-10-05 21:08:50 +0900

    Fix Su

--- index.js
+++ index.js
@@ -854,20 +854,45 @@
     let parts = str.split(".");
     let int_str = parts[0];
     let decimal_str = parts[1];
-    console.info(typeof int_str);
-
     if(int_str){
       const int0 = int_str.match(/0/g);
-      if(int0.length === int_str.length){
+      if(int0 && int0.length === int_str.length){
         int_str = "0";
+      }
+      let new_int_str = "";
+      let start_zero = true;
+      for(let i = 0; i <int_str.length; i++){
+        if(int_str[i] !== "0" || !start_zero){
+          start_zero = false;
+          new_int_str += int_str[i];
+        }
+      }
+      if(!new_int_str){
+        int_str = "0";
+      }else{
+        int_str = new_int_str;
       }
     }
 
     if(decimal_str){
       const dec0 = decimal_str.match(/0/g);
-      if(dec0.length === decimal_str.length){
+      if(dec0 && dec0.length === decimal_str.length){
         decimal_str = "0";
       }
+      let end_zero = true;
+      let new_decimal_str = "";
+      for(let i = decimal_str.length - 1; i >= 0; i--){
+        if(decimal_str[i] !== "0" || !end_zero){
+          end_zero = false;
+          new_decimal_str = decimal_str[i] + new_decimal_str;
+        }
+      }
+      if(!new_decimal_str){
+        decimal_str = "0";
+      }else{
+        decimal_str = new_decimal_str;
+      }
+
     }
 
     let denominator = [1];
