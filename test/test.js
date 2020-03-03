57de7a8ee9da7ed73a4c056fdadefaa0e49d6e2e
syagawa <syagawapp@gmail.com>
2020-03-03 21:07:21 +0900

    Fix test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,11 @@
 const assert = require('assert');
 const app = require("../app.js");
+const s = global.s;
+
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
     it("test", () =>{
-      const one = app.makeSu(1);
+      const one = s.makeSu(1);
       assert.equal(one.getString(), "1");
     })
   });
