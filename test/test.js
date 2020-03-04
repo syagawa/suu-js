efdcc216ad0819e8157990f0ebb3fd945e9022f7
syagawa <syagawapp@gmail.com>
2020-03-04 12:41:08 +0900

    Add test

--- test/test.js
+++ test/test.js
@@ -2,11 +2,22 @@ const assert = require('assert');
 const app = require("../app.js");
 const s = global.s;
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it("test", () =>{
+describe('Su', function() {
+  describe('makeSu', function() {
+    it("zero", () =>{
+      const zero = s.makeSu(0);
+      assert.equal(zero.getString(), "0");
+    });
+    it("one", () =>{
       const one = s.makeSu(1);
       assert.equal(one.getString(), "1");
+    });
+  });
+  describe('add', function() {
+    it("1 + 1 = 2", () =>{
+      const a = s.makeSu(1);
+      const b = s.makeSu(1);
+      assert.equal(a.add(b).getString(), "2");
     })
   });
 });
