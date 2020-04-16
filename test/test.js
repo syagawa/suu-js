1dcc528af484992b915957fb7a9865a3f3db465b
syagawa <syagawapp@gmail.com>
2020-04-16 19:57:26 +0900

    Add test

--- test/test.js
+++ test/test.js
@@ -463,6 +463,19 @@ describe('Su', function() {
       assert.equal(res.fraction.denominator[1], 0);
     });
 
+    it("3 / 2 = ", () =>{
+      const a = s.makeSu("3");
+      const b = s.makeSu("2");
+      const res = a.division(b);
+      assert.equal(res.getString(), "1");
+      assert.equal(res.integer[0], 1);
+      assert.equal(res.decimal[0], 0);
+      assert.equal(res.negative, false);
+      assert.equal(res.fraction.numerator[0], 1);
+      assert.equal(res.fraction.numerator[1], 0);
+      assert.equal(res.fraction.denominator[0], 1);
+      assert.equal(res.fraction.denominator[1], 0);
+    });
 
 
   });
