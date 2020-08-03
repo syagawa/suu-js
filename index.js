8f00bee10151df572fddb9acc6caa2ee2d10c92a
syagawa <syagawapp@gmail.com>
2020-08-03 20:40:07 +0900

    Add core_new

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import s from "./su.js";
 import SK from "./SK.js";
 import core from "./core.js";
+import core_new from "./core_new.js";
 
 export default {
   s: s,
   S: SK.S,
   K: SK.K,
-  core: core
+  core: core,
+  core2: core_new
 };
