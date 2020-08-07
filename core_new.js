2aa9b665bdb60a52f790b854d0b0f4ace73b87ad
syagawa <syagawapp@gmail.com>
2020-08-07 21:26:05 +0900

    Fix core2.compare

--- core_new.js
+++ core_new.js
@@ -82,10 +82,19 @@ core.compare = function(a, b){
     equal: false
   };
 
-  const a_len = a.array.length;
-  const b_len = b.array.length;
-  const a_str = a.array.join("");
-  const b_str = b.array.join("");
+  const a_array = a.array;
+  const b_array = b.array;
+
+  const a_len = a_array.length;
+  const b_len = b_array.length;
+  const a_str = a_array.join("");
+  const b_str = b_array.join("");
+
+  const a_int_len = a.decimal_index;
+  const b_int_len = b.decimal_index;
+
+  const a_dec_len = a_len - a_int_len;
+  const b_dec_len = b_len - b_int_len;
 
   if(a_len === 1 && a_str === "0" && b_len === 1 && b_str === "0"){
     o.equal = true;
@@ -102,10 +111,49 @@ core.compare = function(a, b){
     return o;
   }
 
+  if(a_int_len > b_int_len){
+    o.small = b;
+    o.large = a;
+    return o;
+  }
+  
+  if(a_int_len < b_int_len){
+    o.small = a;
+    o.large = b;
+    return o;
+  }
 
+  for(let i = 0; i < a_int_len; i++){
+    if(a_array[i] > b_array[i]){
+      o.small = b;
+      o.large = a;
+      return o;  
+    }
+    if(a_array[i] < b_array[i]){
+      o.small = a;
+      o.large = b;
+      return o;  
+    }
+  }
 
+  const dec_len = a_dec_len > b_dec_len ? a_dec_len : b_dec_len;
+  for(let i = 0; i < dec_len; i++){
+    const aa = a_array[a_int_len + i] ? a_array[a_int_len + i] : 0;
+    const bb = b_array[b_int_len + i] ? b_array[b_int_len + i] : 0;
+    if(aa > bb){
+      o.small = b;
+      o.large = a;
+      return o;
+    }
+    if(aa < bb){
+      o.small = a;
+      o.large = b;
+      return o;
+    }
+  }
 
-
+  o.equal = true;
+  return o;
 
 
 
