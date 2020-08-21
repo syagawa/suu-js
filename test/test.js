7000331f0fbb7402e4465337021d8578b073f030
syagawa <syagawapp@gmail.com>
2020-08-21 21:59:12 +0900

    Add test

--- test/test.js
+++ test/test.js
@@ -78,6 +78,14 @@ describe("core", function(){
       assert.equal(res.small, one);
       assert.equal(res.equal, false);
     });
+    it("1, 1", () => {
+      const num1 = core.numToArrayWithDecimal("1");
+      const num2 = core.numToArrayWithDecimal("1");
+      const res = core.compare(num1, num2);
+      assert.equal(res.large, null);
+      assert.equal(res.small, null);
+      assert.equal(res.equal, true);
+    });
     it("0, 0", () => {
       const zero1 = core.numToArrayWithDecimal("0");
       const zero2 = core.numToArrayWithDecimal("0");
@@ -126,6 +134,7 @@ describe("core", function(){
       assert.equal(res.small, num2);
       assert.equal(res.equal, false);
     });
+    
     
   
   });
