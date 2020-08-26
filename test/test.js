2dbe39b7a5acae1e3bf0435ce8a0c06b440b75ea
syagawa <syagawapp@gmail.com>
2020-08-26 22:19:07 +0900

    Add test

--- test/test.js
+++ test/test.js
@@ -120,7 +120,6 @@ describe("core", function(){
       assert.equal(res.equal, false);
     });
 
-
     it("1, 0.1", () => {
       const num1 = core.numToArrayWithDecimal("1");
       const num2 = core.numToArrayWithDecimal("0.1");
@@ -153,6 +152,14 @@ describe("core", function(){
       assert.equal(res.large, null);
       assert.equal(res.small, null);
       assert.equal(res.equal, true);
+    });
+    it("0.1, -0.1", () => {
+      const num1 = core.numToArrayWithDecimal("0.1");
+      const num2 = core.numToArrayWithDecimal("-0.1");
+      const res = core.compare(num1, num2);
+      assert.equal(res.large, num1);
+      assert.equal(res.small, num2);
+      assert.equal(res.equal, false);
     });
     
   
