18941355aab17a5bb3e0cc14f57d5163767428b1
syagawa <syagawapp@gmail.com>
2020-09-01 22:18:01 +0900

    Add test

--- test/test.js
+++ test/test.js
@@ -205,6 +205,15 @@ describe("core", function(){
       assert.equal(res, num1);
     });
   });
+
+  describe("getLarge", function(){
+    it("-1, -2", () => {
+      const num1 = core.numToArrayWithDecimal("-1");
+      const num2 = core.numToArrayWithDecimal("-2");
+      const res = core.getLarge(num1, num2);
+      assert.equal(res, num1);
+    });
+  });
 });
 
 
