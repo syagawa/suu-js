7c2f416fa0a61b17eb45c054a8b6dcbe980bbe96
syagawa <syagawapp@gmail.com>
2020-09-30 21:29:52 +0900

    Add test

--- test/test.js
+++ test/test.js
@@ -319,7 +319,12 @@ describe("core", function(){
       const str = core.numArrayToString(res);
       assert.equal(str, "0");
     });
-
+    
+    it("-1 + -1 = 0", () => {
+      const res = core.add("-1", "-1");
+      const str = core.numArrayToString(res);
+      assert.equal(str, "-2");
+    });
 
   });
 
