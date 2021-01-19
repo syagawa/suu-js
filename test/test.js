e427ab0b934e4a07052566cbdb9f4bb58c466c9b
syagawa <syagawapp@gmail.com>
2021-01-19 20:20:29 +0900

    Add test

--- test/test.js
+++ test/test.js
@@ -740,12 +740,18 @@ describe("multiplication", function(){
     const str = core.numArrayToString(res);
     assert.equal(str, "0");
   });
+
   it("0 x 1 = 0", () => {
     const res = core.multiplication("0", "1");
     const str = core.numArrayToString(res);
     assert.equal(str, "0");
   });
-
+  
+  it("1 x 1 = 1", () => {
+    const res = core.multiplication("1", "1");
+    const str = core.numArrayToString(res);
+    assert.equal(str, "1");
+  });
 });
 
 
