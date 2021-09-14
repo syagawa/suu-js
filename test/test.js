7eb6936f6a5782929856a5ac3c9dd3708ee6dcc1
syagawa <syagawapp@gmail.com>
2021-09-14 10:43:09 +0900

    Fix test

--- test/test.js
+++ test/test.js
@@ -1102,22 +1102,18 @@ describe("division", function(){
 
   it("4 / 2 = 2", () => {
     const res = core.division("4", "2");
-    const quotient = res;
-    const remainder = res.remainder;
-    const pro = core.multiplication(quotient, "2");
-    const sum = core.add(pro, remainder);
-    const str = core.numArrayToString(sum);
-    assert.equal(str, "4");
+    const str = core.numArrayToString(res);
+    const remainder = core.numArrayToString(res.remainder);
+    assert.equal(str, "2");
+    assert.equal(remainder, "0");
   });
 
   it("4 / -2 = -2", () => {
     const res = core.division("4", "-2");
-    const quotient = res;
-    const remainder = res.remainder;
-    const sum = core.add(quotient, remainder);
-    const pro = core.multiplication(sum, "-2");
-    const str = core.numArrayToString(pro);
-    assert.equal(str, "4");
+    const str = core.numArrayToString(res);
+    const remainder = core.numArrayToString(res.remainder);
+    assert.equal(str, "-2");
+    assert.equal(remainder, "-0");
   });
 
   it("-4 / 2 = -2", () => {
