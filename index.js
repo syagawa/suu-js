e09f069c1c3b5cb4abcc7f8875dd70c86574dc69
syagawa <syagawapp@gmail.com>
2021-11-19 12:48:50 +0900

    Add getString to utils.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import s from "./su.js";
 import SK from "./SK.js";
 import core from "./core.js";
 import core_new from "./core_new.js";
+import utils from "./utils.js";
 
 export default {
   s: s,
   S: SK.S,
   K: SK.K,
   core: core,
-  core2: core_new
+  core2: core_new,
+  utils: utils,
 };
--- utils.js
+++ utils.js
@@ -2,4 +2,8 @@ import core from "./core_new.js";
 
 const utils = {};
 
+utils.getString = function(n){
+  return core.numArrayToString(n);
+};
+
 export default utils;
