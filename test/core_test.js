cb4a812d493439a8778e917c6ed4cb4023129c09
syagawa <syagawapp@gmail.com>
2022-04-18 12:34:07 +0900

    Add test

--- test/core_test.js
+++ test/core_test.js
@@ -1774,6 +1774,14 @@ describe("division", function(){
     assert.equal(str, "-1");
     assert.equal(check_result.equal, true);
   });
+  
+  it("1 / 3 = 0.333333", () => {
+    const res = core.division("1", "3");
+    const str = core.numArrayToString(res);
+    const check_result = checkRmainderAndQuotient({ dividend: "1", divisor: "3", remainder: res.remainder, quotient: res});
+    assert.equal(str, "0.3333333333");
+    assert.equal(check_result.equal, true);
+  });
 
 });
 
