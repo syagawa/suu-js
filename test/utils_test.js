0037783837b38ebcdee2b535ac9622ed60ffca3a
syagawa <syagawapp@gmail.com>
2022-05-26 22:06:35 +0900

    Add test

--- test/utils_test.js
+++ test/utils_test.js
@@ -334,6 +334,18 @@ describe("utils", function(){
 
   });
 
+  describe("minimumCommonMultiple", function(){
+    it("1, 1", () => {
+      const num1 = utils.getNumber("1");
+      const num2 = utils.getNumber("1");
+      const res1 = utils.minimumCommonMultiple(num1, num2);
+      assert.equal("1", core.numArrayToString(res1));
+    });
+
+
+
+  });
+
 });
 
 
