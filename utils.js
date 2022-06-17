3dc0a6de04ece0ffef61a0e86a3e4d7840da21f6
syagawa <syagawapp@gmail.com>
2022-06-17 12:36:38 +0900

    Add utils.makeHeptanacciSequence

--- utils.js
+++ utils.js
@@ -355,6 +355,17 @@ utils.makeHexanacciSequence = function(){
   return fibonacciReccurenceRelation({array: [a, b, c, d, e, f], limit: 100});
 };
 
+utils.makeHeptanacciSequence = function(){
+  const a = utils.getNumber("0");
+  const b = utils.getNumber("0");
+  const c = utils.getNumber("0");
+  const d = utils.getNumber("0");
+  const e = utils.getNumber("0");
+  const f = utils.getNumber("0");
+  const g = utils.getNumber("1");
+  return fibonacciReccurenceRelation({array: [a, b, c, d, e, f, g], limit: 100});
+};
+
 utils.makeLucaSequence = function(){
   const n1 = utils.getNumber("2");
   const n2 = utils.getNumber("1");
