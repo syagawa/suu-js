192df1241dc0dc3dad358b3706f28661967a2e95
syagawa <syagawapp@gmail.com>
2022-06-22 12:25:19 +0900

    Add utils.makeDodecanacciSequence

--- utils.js
+++ utils.js
@@ -420,6 +420,22 @@ utils.makeUndecanacciSequence = function(){
   return fibonacciReccurenceRelation({array: [a, b, c, d, e, f, g, h, i, j, k], limit: 100});
 };
 
+utils.makeDodecanacciSequence = function(){
+  const a = utils.getNumber("0");
+  const b = utils.getNumber("0");
+  const c = utils.getNumber("0");
+  const d = utils.getNumber("0");
+  const e = utils.getNumber("0");
+  const f = utils.getNumber("0");
+  const g = utils.getNumber("0");
+  const h = utils.getNumber("0");
+  const i = utils.getNumber("0");
+  const j = utils.getNumber("0");
+  const k = utils.getNumber("0");
+  const l = utils.getNumber("1");
+  return fibonacciReccurenceRelation({array: [a, b, c, d, e, f, g, h, i, j, k, l], limit: 100});
+};
+
 utils.makeLucaSequence = function(){
   const n1 = utils.getNumber("2");
   const n2 = utils.getNumber("1");
