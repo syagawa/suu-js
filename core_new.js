008924d551cac2ec10fd6674bbbc339c3e385f0e
syagawa <syagawapp@gmail.com>
2022-08-30 12:40:36 +0900

    Change name core_new.getError => core_new.makeError

--- core_new.js
+++ core_new.js
@@ -1,6 +1,6 @@
 const core = {};
 
-core.getError = function({message, variable}){
+core.makeError = function({message, variable}){
   try{
     const str = `${message}, ${variable.toString()}`
     const error = new Error(str);
@@ -23,7 +23,7 @@ const isNumber = function(n){
 
 core.moldNumArray = function({ array, negative, decimal_index }){
   if(!array){
-    return core.getError({ message: "array is not exists", variable: array});
+    return core.makeError({ message: "array is not exists", variable: array});
   }
   try{
     while(decimal_index < array.length && array[array.length - 1] === 0){
@@ -52,7 +52,7 @@ core.moldNumArray = function({ array, negative, decimal_index }){
 
     return o;
   }catch(e){
-    return core.getError({message: e.message, variable: array});
+    return core.makeError({message: e.message, variable: array});
   }
 
 };
