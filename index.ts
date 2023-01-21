6431245cf805185e4c2569914d3ce37773f5054d
syagawa <syagawapp@gmail.com>
2023-01-21 20:56:44 +0900

    Add typescript settings

---
+++ index.ts
@@ -0,0 +1,16 @@
+import s from "./su.js";
+import SK from "./SK.js";
+import core_old from "./core_old.js";
+import core from "./core.js";
+import utils from "./utils.js";
+import doc from "./doc.js";
+
+export default {
+  s: s,
+  S: SK.S,
+  K: SK.K,
+  core: core,
+  core_old: core_old,
+  utils: utils,
+  doc: doc,
+};
---
+++ webpack.config.ts.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+let ENV = process.env.NODE_ENV;
+console.log(ENV);
+const MODE = process.env.APP_MODE;
+if(ENV !== "production"){
+  ENV = "development";
+}
+
+module.exports = {
+  mode: ENV,
+  entry: [
+    // "core-js/modules/es6.promise",
+    // "core-js/modules/es6.array.iterator",
+    "./index.ts"
+  ],
+  output: {
+    filename: "app2.js",
+    path: path.resolve(__dirname, "./"),
+    library: 'su',
+    libraryExport: "default",
+    libraryTarget: "umd",
+    globalObject: "this"
+  },
+  module: {
+    rules: [
+      // {
+      //   enforce: "pre",
+      //   test: /\.js$/,
+      //   exclude: /(node_modules)/,
+      //   loader: "eslint-loader",
+      //   options: {
+      //     fix: false,
+      //     formatter: "stylish" //"codeframe"
+      //   }
+      // },
+      {
+        test: /\.ts$/,
+        exclude: /(node_modules)/,
+        loader: "ts-loader",
+        // options: {
+        //   presets: ["@babel/preset-env"]
+        // }
+      }
+    ]
+  },
+  plugins: [
+    new ESLintPlugin({
+      fix: false,
+      formatter: "stylish" //"codeframe"
+    })
+  ],
+  resolve: {},
+};
+
+if (ENV !== 'production') {
+  module.exports.devtool = 'inline-source-map';
+}
