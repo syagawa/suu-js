8ad97126948137cdf4c24a56e39ed00f4195fba5
syagawa <syagawapp@gmail.com>
2023-02-08 12:36:29 +0900

    Add CompareObject to interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -5,6 +5,10 @@ export type MoldNumArray = {
   is_num_array: boolean
 }
 
+export type CompareObject = {
+  small: boolean | null
+  large: boolen | null
+  equal: boolean | null
+}
 
 type SuuNumber = MoldNumArray
-
