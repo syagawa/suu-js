3667865f697b2cfde5c6286f50e3a2bcdfbcde64
syagawa <satoruygw@gmail.com>
2023-05-10 12:35:25 +0900

    Add test

--- test/utils_test.js
+++ test/utils_test.js
@@ -146,6 +146,14 @@ describe("utils", function(){
     assert.equal(res2, true);
   });
 
+  it("exponentiate -2.5^3", () => {
+    const base = utils.getNumber("-2.5");
+    const ex = utils.getNumber("3");
+    const res1 = utils.exponentiate(base, ex);
+    const res2 = utils.isEqual(res1, "-15.625");
+    assert.equal(res2, true);
+  });
+
   it("exponentiate -2^2", () => {
     const base = utils.getNumber("-2");
     const ex = utils.getNumber("2");
