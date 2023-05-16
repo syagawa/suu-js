5853089cbeb6b58e130b9d3aef2e87bc6e3bf527
syagawa <syagawapp@gmail.com>
2023-05-16 12:32:52 +0900

    Fix test

--- test/utils_test.js
+++ test/utils_test.js
@@ -173,7 +173,7 @@ describe("utils", function(){
     const base = utils.getNumber("0");
     const ex = utils.getNumber("0");
     const res1 = utils.exponentiate(base, ex);
-    const res2 = utils.isEqual(res1, "0");
+    const res2 = utils.isEqual(res1, "1");
     assert.equal(res2, true);
   });
 
