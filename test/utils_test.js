d3168087f3a6fc4fbc9196736e8c27e13f2d4a81
syagawa <syagawapp@gmail.com>
2023-05-18 12:35:53 +0900

    Fix test

--- test/utils_test.js
+++ test/utils_test.js
@@ -201,8 +201,6 @@ describe("utils", function(){
     assert.equal(res2, true);
   });
 
-
-
   it("exponentiate -2.5^3", () => {
     const base = utils.getNumber("-2.5");
     const ex = utils.getNumber("3");
@@ -224,15 +222,6 @@ describe("utils", function(){
     const ex = utils.getNumber("-2");
     const res1 = utils.exponentiate(base, ex);
     const res2 = utils.isEqual(res1, "1");
-    assert.equal(res2, true);
-  });
-  
-  
-  it("exponentiate -2.5^3", () => {
-    const base = utils.getNumber("-2.5");
-    const ex = utils.getNumber("3");
-    const res1 = utils.exponentiate(base, ex);
-    const res2 = utils.isEqual(res1, "-15.625");
     assert.equal(res2, true);
   });
 
