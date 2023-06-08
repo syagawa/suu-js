83b3470b0efe0fde7d7e2e7462b52accc7c7e136
syagawa <syagawapp@gmail.com>
2023-06-08 22:39:15 +0900

    Update index.js

--- old/index.js
+++ old/index.js
@@ -1,9 +1,9 @@
-import s from "./su.js";
-import SK from "./SK.js";
+import s from "../su.js";
+import SK from "../SK.js";
 import core_old from "./core_old.js";
 import core from "./core.js";
 import utils from "./utils.js";
-import doc from "./doc.js";
+import doc from "../doc.js";
 
 export default {
   s: s,
