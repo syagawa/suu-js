8cdfaa98669bfcf7816f044fad0eb49e594e1198
syagawa <syagawapp@gmail.com>
2023-08-11 20:51:58 +0900

    Add test

--- test/utils_test.js
+++ test/utils_test.js
@@ -142,7 +142,6 @@ describe("utils", function(){
     assert.equal(small, num2);
   });
 
-  
   it("getSmall '' is null", () => {
     const res = utils.getSmall("", "");
     assert.equal(null, res);
@@ -158,6 +157,13 @@ describe("utils", function(){
   it("isEqual 1.0 = 1 => true", () => {
     const num1 = utils.getNumber("1.0");
     const num2 = utils.getNumber("1");
+    const res = utils.isEqual(num1, num2);
+    assert.equal(res, true);
+  });
+
+  it("isEqual -1 = -1 => true", () => {
+    const num1 = utils.getNumber("-1");
+    const num2 = utils.getNumber("-1");
     const res = utils.isEqual(num1, num2);
     assert.equal(res, true);
   });
