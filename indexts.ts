7e1ae4ce924a78fc39669306b3f1c90509263580
syagawa <syagawapp@gmail.com>
2024-01-20 21:59:06 +0900

    Move files

--- indexts.ts
+++ indexts.ts
@@ -1,14 +1,9 @@
-import s from "./su.js";
-import SK from "./SK.js";
 import core from "./corets";
 import utils from "./utilsts";
 import doc from "./docts";
 import constants from "./constants.js"
 
 export default {
-  s: s,
-  S: SK.S,
-  K: SK.K,
   core: core,
   utils: utils,
   doc: doc,
