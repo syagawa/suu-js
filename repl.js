eecf390cc79e29fef621c89f548a23408a2def96
syagawa <satoruygw@gmail.com>
2024-01-25 12:45:21 +0900

    Add execut to repl.js

--- repl.js
+++ repl.js
@@ -1,6 +1,17 @@
 const repl = require("node:repl");
 const app = require("./app_new.js");
 
+const execute = async (method, params) => {
+  const d = new Date();
+  const id = String(d.getTime());
+  console.time(id);
+  const p = new Promise((resolve) => {
+    return resolve(method(params));
+  });
+  await p;
+  console.timeEnd(id);
+};
+
 const replServer = repl.start({
   prompt: "> ",
 });
