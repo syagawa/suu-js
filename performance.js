a00bb96a07cfe7f2858b395a3e32fead19ebdf24
syagawa <syagawapp@gmail.com>
2024-05-06 21:46:18 +0900

    Update performance.js

--- performance.js
+++ performance.js
@@ -1,6 +1,8 @@
 const app = require("./app.js");
 
-const func = function(){
+let counter = 0;
+
+const func1 = function(){
 
   for(let i = 0; i < 100; i++){
     app.core.add("1", "1");
@@ -8,8 +10,17 @@ const func = function(){
 
 };
 
-const main = function(){
-  const id = "test1"
+const func2 = function(){
+
+  for(let i = 0; i < 100; i++){
+    1 + 1;
+  }
+
+};
+
+const execute = function(func){
+  counter++;
+  const id = `test-${String(counter)}`;
   performance.mark("testStart");
   func();
   performance.mark("testEnd");
@@ -23,8 +34,14 @@ const main = function(){
   const results = performance.getEntriesByName(id);
 
   console.log(results[0]);
+};
 
+const main = function(){
+
+  execute(func1);
+  execute(func2);
 
 };
+
 
 main();
