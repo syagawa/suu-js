ea0720f3d02b6bdb00b1ca9645d3b7d275ac1c6c
syagawa <satoruygw@gmail.com>
2024-05-17 12:37:54 +0900

    Fix loopCount in performance.js

--- performance.js
+++ performance.js
@@ -72,9 +72,8 @@ const funcs = {
 
 
 
-const execute = function({func, name, a, b}){
+const execute = function({func, name, a, b, loopCount}){
   counter++;
-  const loopCount = 100;
   const id = `${name}-loop_count_${String(loopCount)}`;
   performance.mark("testStart");
   func(a, b, loopCount);
@@ -96,9 +95,12 @@ const main = function(obj){
   const argv = process.argv;
   const a_ = Number(process.argv[2]);
   const b_ = Number(process.argv[3]);
+  const loopCount_ = Number(process.argv[4]);
 
   const a = a_ ? String(a_) : "1";
   const b = b_ ? String(b_) : "1";
+
+  const loopCount = loopCount_ ? loopCount_ : 100;
 
   let count = 0;
   for(let targetname in obj){
@@ -107,7 +109,7 @@ const main = function(obj){
       count++;
       const name = `${targetname}-${key}-${count}`;
       const func = target[key];
-      execute({func, name, a, b});
+      execute({func, name, a, b, loopCount});
     }
   }
 
