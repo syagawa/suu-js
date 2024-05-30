5b734908df940971c3191c94b9985c8f761c0904
syagawa <satoruygw@gmail.com>
2024-05-30 12:51:12 +0900

    Add random.getRandomNumberByMiddleSquareMethod

--- random.ts
+++ random.ts
@@ -1,16 +1,29 @@
 import core from "./core";
+import utils from "./utils";
 
 import { SuuNumber } from "./interfaces";
 
 const random:any = {};
 
 random.getNotRandomNumber = () => {
-  return core.numToArrayWithDecimal("1");
+  return core.getSuuNumber("1");
 };
 
 random.getRandomNumber = (seed: any) => {
   const num = Math.random();
   return core.getSuuNumber(num);
+};
+
+random.getRandomNumberByMiddleSquareMethod = () => {
+  const seed = "1234";
+  const first = core.getSuuNumber(seed);
+  const res = utils.square(first);
+  let second = res.array.slice(2, 6).join("");
+  if(res.array.length === 7){
+    second = res.array.slice(1, 5).join("");
+  }
+  const secondnum = core.getSuuNumber(second);
+  return secondnum;
 };
 
 
