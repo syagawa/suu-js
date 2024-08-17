024b091918b48ff6d72c5387ca74887ebfc97fe1
syagawa <syagawapp@gmail.com>
2024-08-17 21:10:44 +0900

    Add test

--- test/utils_test.js
+++ test/utils_test.js
@@ -2399,7 +2399,10 @@ describe("utils", function(){
       const res = utils.getFermatNumber("0");
       assert.equal("3", core.numArrayToString(res));
     });
-
+    it("1 => 5", () =>{
+      const res = utils.getFermatNumber("1");
+      assert.equal("5", core.numArrayToString(res));
+    });
 
 
   });
