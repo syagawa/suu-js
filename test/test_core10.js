bbd019d567c4770287c7dcde67f501b4a0db3e44
syagawa <syagawapp@gmail.com>
2026-02-25 03:44:45 +0000

    fix: add test

--- test/test_core10.js
+++ test/test_core10.js
@@ -6,14 +6,24 @@ console.log(calc);
 
 describe("calc", function(){
 
-  describe("numToArrayWithDecimal", function(){
+  describe("+", function(){
     it("1 + 1 = 2", () => {
       const res = calc("1", "+", "1");
       assert.equal(res, "2");
     });
+    it("10 + 1 = 11", () => {
+      const res = calc("10", "+", "1");
+      assert.equal(res, "11");
+    });
+  });
+  describe("-", function(){
     it("1 - 1 = 0", () => {
-      const res = calc("1", "-", "1");
+      const res = calc("1", "0", "1");
       assert.equal(res, "0");
+    });
+    it("10 - 1 = 9", () => {
+      const res = calc("10", "-", "1");
+      assert.equal(res, "9");
     });
   });
 });
