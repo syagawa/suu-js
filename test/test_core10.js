a4efc093bfa6e7db19a76cadf4f45b46753571f7
syagawa <syagawapp@gmail.com>
2026-08-03 03:42:03 +0000

    test: add test

--- test/test_core10.js
+++ test/test_core10.js
@@ -362,19 +362,18 @@ describe("calc", function(){
       const res = calc("100", "-", "1");
       assert.equal(res, "99");
     });
-
     it("1.0 - 100 = -99", () => {
       const res = calc("1.0", "-", "100");
       assert.equal(res, "-99");
     });
-    // it("1 + 100.0 = 101", () => {
-    //   const res = calc("1", "+", "100.0");
-    //   assert.equal(res, "101");
-    // });
-    // it("1.0 + 100.0 = 101", () => {
-    //   const res = calc("1.0", "+", "100.0");
-    //   assert.equal(res, "101");
-    // });
+    it("1 - 100.0 = -99", () => {
+      const res = calc("1", "-", "100.0");
+      assert.equal(res, "-99");
+    });
+    it("1.0 - 100.0 = -99", () => {
+      const res = calc("1.0", "-", "100.0");
+      assert.equal(res, "-99");
+    });
 
 
     it("0.1 + 0 = 0.1", () => {
